2690e3096808221b4061145d904887226b761e2d
Jasmine <jasminehweying@gmail.com>
2023-05-16 09:51:33 +0800

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Jasmine Lai Hweeying on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
